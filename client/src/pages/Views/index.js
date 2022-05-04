ee878214318b0de60a83d006ff3efb5a35cbe721
Riley Smith <gordonrileysmith@gmail.com>
2022-05-04 14:42:52 -0600

    trying

--- client/src/pages/Views/index.js
+++ client/src/pages/Views/index.js
@@ -16,8 +16,8 @@ export default function Resume() {
     const [applications, setApplications] = useState()
 
     const { loading, data } = useQuery(QUERY_APPS);
-    // const apps = data.applications || [];
-    console.log(data)
+    const apps = data.applications || [];
+    console.log(apps)
     //  const [getApps, { loading, error, data }] = useQuery(QUERY_APPS, {
     //     variables: {
     //         date: '05 02 2022'
